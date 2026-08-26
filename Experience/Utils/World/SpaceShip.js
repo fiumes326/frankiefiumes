7b71ca1ad693ba31f8dfff56356851fc44242f1b
Frank <frank.fiumefreddo@fisrev.com>
2026-08-26 14:49:31 -0400

    beginning to add spaceship

--- Experience/Utils/World/SpaceShip.js
+++ Experience/Utils/World/SpaceShip.js
@@ -1,3 +1,15 @@
+import Experience from "../../Experience";
+
 export default class SpaceShip {
+
+    constructor(){
+        this.experience = new Experience()
+        this.scene = this.experience.scene
+        this.resources = this.experience.resources
+    }
+
+    setMesh(){
+
+    }
 
 }
