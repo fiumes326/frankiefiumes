2276cf7e4d6cd315289846de0330dd50ce0f2e64
Frank <frank.fiumefreddo@fisrev.com>
2026-08-31 07:27:21 -0400

    adding Earth text to the minimap

--- Experience/World/Earth.js
+++ Experience/World/Earth.js
@@ -1,13 +1,15 @@
 import * as THREE from 'three'
 import Experience from '../Experience'
-
+import { Text } from 'troika-three-text'
 export default class Earth {
     constructor() {
         this.experience = new Experience()
         this.scene = this.experience.scene
         this.resource = this.experience.resource
         this.rotation = 0
+        this.miniMapText = new Text()
         this.setMesh()
+        this.setMiniMapText()
     }
 
     setMesh() {
@@ -21,6 +23,20 @@ export default class Earth {
         this.earthGroup.position.set(0,-10,0)
         this.earthGroup.traverse((object) => object.layers.enable(1))
         this.scene.add(this.earthGroup)
+    }
+
+    setMiniMapText(){
+        this.miniMapText.text = "EARTH"
+        this.miniMapText.position.set(0, 0, 3)
+        this.miniMapText.rotation.x = -Math.PI / 2
+        this.miniMapText.anchorX = 'center'
+        this.miniMapText.anchorY = 'middle'
+        this.miniMapText.color = 0xFFFFFF
+        this.miniMapText.outlineColor = 0x000000
+        this.miniMapText.outlineWidth = 0.08
+        this.miniMapText.fontSize = 2
+        this.miniMapText.layers.set(1)
+        this.scene.add(this.miniMapText)
     }
 
     update(){
